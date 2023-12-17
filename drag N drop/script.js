1859442e45b86cf655b118a1fc26ae8de465aca5
Your Name <karub0005@gmail.com>
2023-12-17 20:30:44 +0545

    drag n drop::

--- drag N drop/script.js
+++ drag N drop/script.js
@@ -1,2 +1,29 @@
-let list = document.getElementsByClassName("list")
+let lists = document.getElementsByClassName("list");
+let rightBox = document.getElementById("right");
+let leftBox = document.getElementById("left");
+let selected = null;
 
+for (let list of lists) {
+  list.addEventListener("dragstart", function (e) {
+    selected = e.target;
+    console.log(selected);
+
+    rightBox.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+
+    rightBox.addEventListener("drop", function (e) {
+      rightBox.appendChild(selected);
+      selected = null;
+    });
+    // left-box
+    leftBox.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+
+    leftBox.addEventListener("drop", function (e) {
+      leftBox.appendChild(selected);
+      selected = null;
+    });
+  });
+}
